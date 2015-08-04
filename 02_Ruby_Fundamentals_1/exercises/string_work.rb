03dd7e00b6cb7f3fd6af374f9f9b664a3d6e942b
Alexandra Pisano <pisano.alexandra@gmail.com>
2015-08-03 20:19:50 -0700

    completed string work

---
+++ 02_Ruby_Fundamentals_1/exercises/string_work.rb
@@ -0,0 +1,27 @@
+greeting = "Hello, World"
+name = "Ali"
+bewd_class = "Back End Web Development"
+
+# determines number of characters in a string
+# puts greeting.length
+puts "#{name} has #{name.length} characters"
+
+# adds something to beginning of string
+# puts name.prepend("The ")
+puts "I added The to this #{name.prepend("The ")}"
+
+# puts characters in a string into an array
+puts name.chars
+puts "This is my last name as an array
+#{last_name.chars}. This is of class #{last_name.chars.class}"
+
+
+# adds to end of string
+name_2 = name << " Cool"
+puts "I prepended cool to this #{name_2}"
+
+# removes white space at end of string
+# puts bewd_class.strip
+puts "I stripped this #{bewd_class.strip}"
+
+
