b11f010ea34e8e09762ecedfebda75c073e8753d
Alexandra Pisano <pisano.alexandra@gmail.com>
2015-09-23 16:17:27 -0700

    Tried to add reviews

--- 15_Rails_Intro_Front_End_Continued/code_along/my_music_app/config/routes.rb
+++ 15_Rails_Intro_Front_End_Continued/code_along/my_music_app/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   resources :artists
+    resources :reviews
   #establish a custom route
   get 'about', to: "static_pages#about", as: "about"
   get 'test', to: "static_pages#test", as: "test"
